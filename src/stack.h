e57585b867b71e052ef425c731a1f498971a0d33
Gustavo Barbosa <gustavobarbosa-18@hotmail.com>
2023-06-24 15:48:50 -0300

    feat: add stack interface

---
+++ src/stack.h
@@ -0,0 +1,29 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct Stack {
+    void *data;
+    int *top;
+    int size;
+} Stack;
+
+Stack create_stack();
+
+// Retorna 1 para sucesso e 0 para erro
+int push(Stack *stack, int value);
+
+// Retorna o valor removido.
+// Caso a lista esteja vazia, será retornado zero.
+// Sim, zero pode ser um valor removido, então se for preciso, basta verificar
+// se a lista está vazia antes de tentar remover.
+int pop(Stack *stack);
+
+// Retorna o valor do top da pilha
+// Comportamento de retorno zero semelhante a função pop.
+int top(Stack *stack);
+
+int is_empty(Stack *stack);
+
+void print_stack(Stack *stack);
+
+#endif
